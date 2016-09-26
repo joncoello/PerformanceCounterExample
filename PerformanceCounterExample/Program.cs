0c8b709f61897863630251d01ae2abd1abe6c602
Jon.Coello <Jon.Coello@WLKCND4071HPV>
2016-09-26 21:27:18 +0100

    Add project files.

---
+++ PerformanceCounterExample/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PerformanceCounterExample {
+    class Program {
+        static void Main(string[] args) {
+
+            var bgWk = new BackgroundWorker();
+            bgWk.WorkerSupportsCancellation = true;
+            bgWk.DoWork += (s, e) => {
+
+                var category = new PerformanceCounterCategory("Network Interface");
+                var instances = category.GetInstanceNames();
+                var instance = instances.FirstOrDefault(i => i.ToLower().Contains("wireless"));
+                var counters = category.GetCounters(instance);
+
+                var c1 = counters.FirstOrDefault(c => c.CounterName == @"Bytes Total/sec");
+                var c2 = counters.FirstOrDefault(c => c.CounterName == @"Current Bandwidth");
+                
+                while (!bgWk.CancellationPending) {
+
+                    Thread.Sleep(1000);
+
+                    var bytesPerSecond = Convert.ToDecimal(c1.RawValue);
+                    var bandwidth = Convert.ToDecimal(c2.RawValue);
+
+                    var utilization = bytesPerSecond / bandwidth;
+
+                    Console.WriteLine("Value is at " + utilization.ToString());
+
+                }
+                
+            };
+            bgWk.RunWorkerAsync();
+
+            Console.ReadKey();
+
+            bgWk.CancelAsync();
+
+        }
+    }
+}
